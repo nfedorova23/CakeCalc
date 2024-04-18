d06d71ee5e9dec602b72c7bf8e97e82122ea2c93
nadfed23 <nadezdafedorova90867@gmail.com>
2024-04-19 02:17:31 +0300

    Add ui

--- app/src/main/java/com/nfedorova/cakecal/MainActivity.kt
+++ app/src/main/java/com/nfedorova/cakecal/MainActivity.kt
@@ -2,10 +2,43 @@ package com.nfedorova.cakecal
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.NavController
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.nfedorova.cakecal.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var navController: NavController
+    private lateinit var navView: BottomNavigationView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        navView = binding.bottomNavigationView
+        navController = findNavController(R.id.fragmentContainerView)
+        val appBarConfiguration = AppBarConfiguration(setOf(
+            R.id.recipes_menu, R.id.calculate_menu, R.id.settings_menu))
+        val appBarConfigurationOne = AppBarConfiguration(navController.graph)
+        setSupportActionBar(binding.toolbar)
+        binding.toolbar.setupWithNavController(navController, appBarConfigurationOne)
+        navView.setupWithNavController(navController)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return item.onNavDestinationSelected(navController) || super.onOptionsItemSelected(item)
     }
 }
---
+++ app/src/main/java/com/nfedorova/cakecal/presentation/ui/calculate/CalculateFragment.kt
@@ -0,0 +1,25 @@
+package com.nfedorova.cakecal.presentation.ui.calculate
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.nfedorova.cakecal.R
+
+
+class CalculateFragment : Fragment() {
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?,
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_calculate, container, false)
+    }
+
+    companion object {
+
+    }
+}
